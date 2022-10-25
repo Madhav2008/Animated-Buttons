5b611c271550b81ac76d931c81d7fbf2fdc29555
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 21:00:37 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -84,7 +84,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 bubblesColor: BubblesColor(
                   dotPrimaryColor: Colors.greenAccent.shade200,
-                  dotSecondaryColor: Colors.deepPurple.shade300,
+                  dotSecondaryColor: Colors.greenAccent.shade300,
                 ),
                 likeCount: numberOfLikes,
                 countPostion: CountPostion.bottom,
