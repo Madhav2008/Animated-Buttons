2c82667ee13d9a0aed3c15e31cebe9820c956b65
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:07:23 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Animated Buttons',debugShowCheckedModeBanner: ,
+      title: 'Animated Buttons',debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
