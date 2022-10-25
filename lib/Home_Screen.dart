b02ea529f649a9831c1c0b97409b27908c1c6ae8
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:30:07 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -170,7 +170,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 countPostion: CountPostion.bottom,
                 likeBuilder: ((isTapped) {
                   return Icon(
-                    Icons.star,
+                    Icons.star_border,
                     color: isTapped ? Colors.yellow : Colors.grey,
                     size: 80,
                   );
