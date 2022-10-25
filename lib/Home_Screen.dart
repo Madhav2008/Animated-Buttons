6e440db12c3a097954bead8e03293590e8b9ec85
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 23:20:04 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -273,9 +273,10 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     );
                   },
-                ),const SizedBox(
-                width: 20,
-              ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
                 LikeButton(
                   size: 80,
                   circleColor: const CircleColor(
