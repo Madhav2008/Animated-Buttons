267110f64571c7c86cde5b9b365e0a7ba07a3efb
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:22:54 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -193,7 +193,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 likeBuilder: ((isTapped) {
                   return Icon(
                     Icons.mail,
-                    color: isTapped ? gradient: LinearGradient(
+                    color: isTapped ? LinearGradient(
                colors: [Colors.purple, Colors.blue],
               begin: Alignment.bottomLeft,
                end: Alignment.topRight,
