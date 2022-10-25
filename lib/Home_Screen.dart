aac06a59d124750e67d1bd0fe74fa01fab141575
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 23:11:53 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -203,7 +203,8 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ],
           ),
-          Row(mainAxisAlignment: MainAxisAlignment.center,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               LikeButton(
                 size: 80,
