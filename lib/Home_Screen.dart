7fe2040cf2828d3ebc36e077707ac03e59bbc021
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:37:25 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -47,12 +47,13 @@ class _HomeScreenState extends State<HomeScreen> {
                 },
               ),
               LikeButton(
-                size: 80,circleColor:
-              CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
-          bubblesColor: BubblesColor(
-            dotPrimaryColor: Color(0xff33b5e5),
-            dotSecondaryColor: Color(0xff0099cc),
-          ),
+                size: 80,
+                circleColor: CircleColor(
+                    start: Color(0xff00ddff), end: Color(0xff0099cc)),
+                bubblesColor: BubblesColor(
+                  dotPrimaryColor: Color(0xff33b5e5),
+                  dotSecondaryColor: Color(0xff0099cc),
+                ),
                 likeCount: numberOfLikes,
                 countPostion: CountPostion.bottom,
                 likeBuilder: ((isTapped) {
