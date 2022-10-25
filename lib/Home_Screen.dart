f63eb97eb0f6ded8236d18ff27096dd204076b12
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:01:26 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -1,18 +1,46 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int numberOfLikes = 120;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ANIMATED BUTTONS'),
+        title: const Text(
+          'ANIMATED LIKE BUTTON',
+          style: TextStyle(
+            letterSpacing: 3,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: LikeButton(
+          size: 80,
+          likeCount: numberOfLikes,
+          countPostion: CountPostion.bottom,
+          countBuilder: (count, isLiked, text) {
+            return Text(
+              text,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
