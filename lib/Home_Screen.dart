0aa2408bdba36e44cf1a4f633ebca1b95c67eff7
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:02:05 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -18,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'ANIMATED BUTTON',
+          'ANIMATED BUTTONS',
           style: TextStyle(
             letterSpacing: 3,
           ),
