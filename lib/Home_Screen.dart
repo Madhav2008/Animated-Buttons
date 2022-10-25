4dbbe9d5ceb28e6cee57a01c596e4dc3376a764f
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 23:48:09 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -153,7 +153,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       );
                     } else
-                      result = Text(
+                      result = const Text(
                         count! >= 1000
                             ? (count / 1000.0).toStringAsFixed(1) + 'k'
                             : text,
