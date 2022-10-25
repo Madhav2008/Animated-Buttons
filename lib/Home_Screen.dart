1c1bb4c02ff39f5488076ada5487bdac61176048
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:14:12 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -167,7 +167,13 @@ class _HomeScreenState extends State<HomeScreen> {
               LikeButton(
                 size: 80,
                 likeCount: numberOfLikes,
-                countPostion: CountPostion.bottom,
+                countPostion: CountPostion.bottom,likeBuilder: ((isTapped) {
+                  return Icon(
+                    Icons.bookmark,
+                    color: isTapped ? Colors.deepPurple : Colors.grey,
+                    size: 80,
+                  );
+                }),
                 countBuilder: (count, isLiked, text) {
                   return Text(
                     text,
