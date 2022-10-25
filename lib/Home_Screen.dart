781f1e7212b5c59e33edb87973d98b39b0733d90
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:59:32 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -143,7 +143,7 @@ class _HomeScreenState extends State<HomeScreen> {
               LikeButton(
                 size: 80,
                 circleColor: const CircleColor(
-                  start: Colors.deepPurple,
+                  start: Colors.blue,
                   end: Colors.deepPurpleAccent,
                 ),
                 bubblesColor: BubblesColor(
