fad35b5181abf17b793b1de5bb5af74d9c19c6c1
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:09:48 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Animated Buttons',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const HomeScreen(),
     );
