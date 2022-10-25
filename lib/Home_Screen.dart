c9e4a220cda038f48aca2b76427917cbce32a746
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 20:34:56 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -215,7 +215,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 likeBuilder: ((isTapped) {
                   return Icon(
                     Icons.currency_rupee,
-                    color: isTapped ? Colors.amber : Colors.grey,
+                    color: isTapped ? Colors.purple : Colors.grey,
                     size: 80,
                   );
                 }),
