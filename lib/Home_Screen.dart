5e3b5edd80866a655e8ca70ca153a9c5ea3bfcd8
Madhav2008 <aroramadhav65@gmail.com>
2022-10-26 14:29:46 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -23,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(
             letterSpacing: 3,
           ),
-        ),elevation: ,
+        ),elevation: 10,
         centerTitle: true,
       ),
       body: Column(
