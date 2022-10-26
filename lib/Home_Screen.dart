6c53580b1dbf65240a507f68a3d6944c59a77ef7
Madhav2008 <aroramadhav65@gmail.com>
2022-10-26 14:12:01 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -36,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 LikeButton(
                   size: 80,
                   likeCount: numberOfLikes,
-                  countPostion: CountPostion.bottom,animationDuration: Duration(seconds: ),
+                  countPostion: CountPostion.bottom,animationDuration: Duration(seconds: 3),
                   countBuilder: (count, isLiked, text) {
                     return Text(
                       text,
