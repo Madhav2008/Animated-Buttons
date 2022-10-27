9a245445c732e8d1f11f485cc324804ebf110ff8
Madhav2008 <aroramadhav65@gmail.com>
2022-10-27 16:59:44 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -62,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       );
                     }
-                    
+
                     return result;
                   },
                 ),
